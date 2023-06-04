dc0e1737c238cb4584bf8ca5cf9ce1aac70ba0c7
Harshit Pachori <harshitpachori345@gmail.com>
2023-06-04 16:00:01 +0530

    Search in a 2d Matrix

---
+++ SearchInA2DMatrix.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class SearchInA2DMatrix {
+    static boolean searchMatrix(ArrayList<ArrayList<Integer>> mat, int target) {
+        int n = mat.size();
+        int m = mat.get(0).size();
+        int start = 0;
+        int end = (m * n) - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int currEle = mat.get(mid / m).get(mid % m);
+            if (currEle == target) {
+                return true;
+            } else if (currEle < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+}
